e5a3e8e7a683e09eaaf3b99ef81dea2de0fdb7a1
Murad Hossen <idb.1285072@gmail.com>
2025-09-23 15:40:02 +0600

    unsubscription query params

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -27,6 +27,7 @@ import { UserFormInterface } from '../types/user-form.interface';
 export class UserListComponent implements OnInit, OnDestroy {
   private searchSubject$ = new Subject<string>();
   private searchSubscription?: Subscription;
+  private queryParamSubscription?: Subscription;
   displayedUsers: UserInterface[] = [];
   totalUsers: number = 0;
   isBulkUpdate: boolean = false;
@@ -378,23 +379,25 @@ export class UserListComponent implements OnInit, OnDestroy {
   }
 
   private initQueryParam() {
-    this.activatedRoute.queryParams.subscribe((params) => {
-      this.currentPage = +params['page'] || 1;
-      this.itemsPerPage = +params['itemsPerPage'] || 5;
-      this.searchTerm = params['search'] || '';
-      const statusParam = params['status'];
-      if (statusParam && !isNaN(+statusParam)) {
-        this.statusFilter = +statusParam as StatusTypeEnum;
-      } else {
-        this.statusFilter = StatusTypeEnum.active;
+    this.queryParamSubscription = this.activatedRoute.queryParams.subscribe(
+      (params) => {
+        this.currentPage = +params['page'] || 1;
+        this.itemsPerPage = +params['itemsPerPage'] || 5;
+        this.searchTerm = params['search'] || '';
+        const statusParam = params['status'];
+        if (statusParam && !isNaN(+statusParam)) {
+          this.statusFilter = +statusParam as StatusTypeEnum;
+        } else {
+          this.statusFilter = StatusTypeEnum.active;
+        }
+        if (params['role'] && params['role'] !== 'all') {
+          this.roleFilter = +params['role'];
+        } else {
+          this.roleFilter = 'all';
+        }
+        this.refreshDisplayedUsers();
       }
-      if (params['role'] && params['role'] !== 'all') {
-        this.roleFilter = +params['role'];
-      } else {
-        this.roleFilter = 'all';
-      }
-      this.refreshDisplayedUsers();
-    });
+    );
   }
 
   private initSearchSubscription() {
@@ -503,5 +506,6 @@ export class UserListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.searchSubscription?.unsubscribe();
+    this.queryParamSubscription?.unsubscribe();
   }
 }
